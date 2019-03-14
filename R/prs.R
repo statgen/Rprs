d29c52e6523b291b4e44d9642114ba4bfaf454ef
Daniel Taliun <dtaliun@umich.edu>
2019-03-14 09:24:12 -0400

    Allow multiple P-value thresholds

--- R/prs.R
+++ R/prs.R
@@ -1,4 +1,4 @@
-prs <- function(weights_file, genotypes_file, samples_file, pvalue = 1) {
+prs <- function(weights_file, genotypes_file, samples_file, pvalues = c(1.0)) {
    if (is.null(weights_file)) {
       stop("Provide weights file.")
    }
@@ -8,8 +8,11 @@ prs <- function(weights_file, genotypes_file, samples_file, pvalue = 1) {
 
    weights <- read.table(weights_file, header = TRUE, stringsAsFactors = FALSE, colClasses = c("character", "integer", "character", "character", "numeric", "numeric"))
    weights <- weights[with(weights, order(CHROM, POS, decreasing = FALSE)),] # ensure that positions are sorted 
-   weights <- weights[weights$PVALUE < pvalue, ] # subset to weights which satisfy p-value threshold
-  
+   
+   
+   weights <- weights[weights$PVALUE < max(pvalues), ] # subset to weights which satisfy all p-value thresholds
+   pvalues <- sort(pvalues, decreasing = TRUE)
+
    # set samples to use
    if (!is.null(samples_file)) {
       samples <- readLines(samples_file)
@@ -20,7 +23,7 @@ prs <- function(weights_file, genotypes_file, samples_file, pvalue = 1) {
    genotypes <- new(Genotypes, genotypes_file) # create VCF/BCF/savvy reader
    genotypes$open(samples) # open file for reading and set samples
 
-   individual_prs <- matrix(nrow = length(samples), 1, data = 0, dimnames = list(samples = samples, scores = c("PRS")))
+   individual_prs <- matrix(nrow = length(samples), ncol = length(pvalues), data = 0, dimnames = list(samples = samples, scores = paste("PRS_", pvalues, sep="")))
    for (row_idx in 1:nrow(weights)) {
       weight <- weights[row_idx, "WEIGHT"]
       if (weight >= 0) {
@@ -32,6 +35,7 @@ prs <- function(weights_file, genotypes_file, samples_file, pvalue = 1) {
          pa <- weights[row_idx, "EA"]
          risk <- -1 * weight
       }
+      pvalue <- weights[row_idx, 'PVALUE']
       if (row_idx %% 10000 == 0) {
          message(sprintf("Processed %d/%d variants", row_idx, nrow(weights)))
       }
@@ -39,7 +43,13 @@ prs <- function(weights_file, genotypes_file, samples_file, pvalue = 1) {
       if (nrow(dosage) == 0) {
          next
       }
-      individual_prs <- individual_prs + risk * dosage
+      for (i in seq_along(pvalues)) {
+         if (pvalue < pvalues[i]) {
+            individual_prs[, i] <- individual_prs[, i] + risk * dosage
+         } else {
+            break
+         }
+      }
    }
    message("Done")
    d <- cbind(IID = dimnames(individual_prs)$samples, data.frame(individual_prs, row.names = NULL))
